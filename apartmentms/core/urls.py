92be95454a69f02e0c20ccc35c3fa2af4b60c0da
saahithjohn <saahithjohn@gmail.com>
2025-08-05 10:52:45 +0000

    new file:   apartmentms/core/__pycache__/forms.cpython-312.pyc 	modified:   apartmentms/core/__pycache__/urls.cpython-312.pyc 	modified:   apartmentms/core/__pycache__/views.cpython-312.pyc 	modified:   apartmentms/core/urls.py 	modified:   apartmentms/core/views.py

--- apartmentms/core/urls.py
+++ apartmentms/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('messages/', views.message_list, name='message_list'),
+    path('messages/create/', views.message_create, name='message_create'),
+    path('messages/<int:pk>/', views.message_detail, name='message_detail'),
+    path('messages/<int:pk>/edit/', views.message_update, name='message_update'),
+    path('messages/<int:pk>/delete/', views.message_delete, name='message_delete'),
 ]
--- apartmentms/core/views.py
+++ apartmentms/core/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Message
+from .forms import MessageForm
 
 def home(request):
     return render(request, 'core/home.html')
+
+@login_required
+def message_list(request):
+    messages = Message.objects.all()
+    return render(request, 'core/message_list.html', {'messages': messages})
+
+@login_required
+def message_detail(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    return render(request, 'core/message_detail.html', {'message': message})
+
+@login_required
+def message_create(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:message_list')
+    else:
+        form = MessageForm()
+    return render(request, 'core/message_form.html', {'form': form})
+
+@login_required
+def message_update(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    if request.method == 'POST':
+        form = MessageForm(request.POST, instance=message)
+        if form.is_valid():
+            form.save()
+            return redirect('core:message_detail', pk=pk)
+    else:
+        form = MessageForm(instance=message)
+    return render(request, 'core/message_form.html', {'form': form})
+
+@login_required
+def message_delete(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    if request.method == 'POST':
+        message.delete()
+        return redirect('core:message_list')
+    return render(request, 'core/message_confirm_delete.html', {'message': message})
